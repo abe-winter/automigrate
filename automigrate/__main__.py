1b5c211f75151410ace36480dc25e8deab462570
Abe Winter <abe@none>
2019-03-29 21:19:40 -0400

    command-line for non-initial changes

--- automigrate/__main__.py
+++ automigrate/__main__.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 "entrypoint for automigrate command line"
+# example:
+# automig 218dd2c 'test/schema/*.sql' --initial
+# automig 218dd2c...b5b40ce 'test/schema/*.sql'
+# automig 218dd2c...HEAD 'test/schema/*.sql'
 
-import argparse
-from .lib import diffing
+import argparse, git
+from datetime import datetime
+from .lib import ref_diff, githelp
 
 PARSER = argparse.ArgumentParser(__doc__)
-PARSER.add_argument('git_diff', help="git sha or delta")
+PARSER.add_argument('ref', help="single git ref (i.e. sha) or sha1...sha2")
 PARSER.add_argument('glob', help="glob to grab paths, typicaly 'schema/*.sql' or something")
 PARSER.add_argument('--initial', action='store_true', help="is this an initial commit (i.e. create metadata)")
 PARSER.add_argument('--transactional', action='store_true', help="definitely enable this on DBs that support transactional DDL")
@@ -14,7 +19,19 @@ def main():
   args = PARSER.parse_args()
   if args.transactional:
     raise NotImplementedError('transactional DDL not supported yet')
-  raise NotImplementedError
+  rev_tuple = githelp.parse_rev_to_tuple(args.ref)
+  print(f'-- changeset created from {args} at {datetime.now()}')
+  if args.initial:
+    assert len(rev_tuple) == 1, "can't pass a sha range for --initial"
+    raise NotImplementedError
+  else:
+    assert len(rev_tuple) == 2, "must pass a sha range or set --initial"
+    changes = ref_diff.ref_range_diff(git.Repo(), *rev_tuple, args.glob)
+    for sha, tables in changes.items():
+      for table, stmts in tables.items():
+        print(f'-- changes for {sha[:7]}.{table}')
+        for stmt in stmts:
+          print(stmt)
 
 if __name__ == '__main__':
   main()
--- automigrate/lib/githelp.py
+++ automigrate/lib/githelp.py
@@ -2,6 +2,15 @@
 
 import git, fnmatch, os, collections
 
+def parse_rev_to_tuple(rev_string):
+  if '...' in rev_string:
+    left, _, right = rev_string.partition('...')
+    return left, right
+  elif '..' in rev_string:
+    raise ValueError("we don't support two-dot strings -- use a three-dot")
+  else:
+    return rev_string,
+
 def get_paths(tree, pattern, root=()):
   """given tree (a git tree at a desired ref) return list of paths matching pattern
   note: this matches paths that existed at the time of ref, including files that don't exist now and excluding ones that exist on disk but were missing at ref
