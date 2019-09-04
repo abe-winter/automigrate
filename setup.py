a7859fe5f74bbcc0c7537688e53423401992a1a7
Abe Winter <abe@none>
2019-09-04 09:17:05 -0400

    prep for pypi

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ from distutils.core import setup
 setup(
   name='automigrate',
   version='0.0.1',
-  description="",
+  description="Command to diff SQL schemas in git and apply the migrations",
   author="Abe Winter",
   url="https://github.com/abe-winter/automigrate",
   packages=['automigrate'],
   entry_points = {
     'console_scripts': ['automig=automigrate.__main__:main'],
-  }
+  },
+  keywords=['sql', 'migration', 'git', 'diff'],
 )
