977047fb89b0a4a1df3aa332589525e48081a825
abe-winter <abe-winter@users.noreply.github.com>
2019-09-04 09:49:49 -0400

    prep for pypi deployment (#1)
    * rename for pypi name collision
    
    * fix tests
    
    * gitignore for deployment files
    
    * development workflow section
    
    * copy requirements.txt into setup.py
    
    * update names in setup.py
    
    * require py3
    
    * find_packages()

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
-  name='automigrate',
+  name='automig',
   version='0.0.1',
   description="Command to diff SQL schemas in git and apply the migrations",
   author="Abe Winter",
   url="https://github.com/abe-winter/automigrate",
-  packages=['automigrate'],
+  packages=find_packages(include=['automig', 'automig.*']),
   entry_points = {
-    'console_scripts': ['automig=automigrate.__main__:main'],
+    'console_scripts': ['automig=automig.__main__:main'],
   },
   keywords=['sql', 'migration', 'git', 'diff'],
+  install_requires=[
+    'sqlparse==0.3.0',
+    'gitpython==2.1.11',
+    'pyyaml==5.1',
+  ],
+  python_requires='>=3.6', # for format strings
 )
