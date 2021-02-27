48fc5774cba32b3c0b2131aec9bdf748ed23b0f8
Abe Winter <abe-winter@users.noreply.github.com>
2021-02-27 14:52:43 -0500

    create enum history

--- test/schema/sql.sql
+++ test/schema/sql.sql
@@ -3,6 +3,8 @@
 
 create extension if not exists "uuid-ossp";
 
+create type letters as enum ('a', 'b');
+
 create table t1 (a int primary key, b text);
 create index t1a on t1 (a);
 create table t2 (a int primary key);
